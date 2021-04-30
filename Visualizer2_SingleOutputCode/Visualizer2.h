bba80441f0e924e0a9597a3a4b88bf169f65aee2
Danny Flynn <p.daniel.flynn@gmail.com>
2021-04-30 12:54:01 -0400

    add multiple color scheme support and fading color

--- Visualizer2_SingleOutputCode/Visualizer2.h
+++ Visualizer2_SingleOutputCode/Visualizer2.h
@@ -1,6 +1,6 @@
 // Visualizer2.h
 // Author: Danny Flynn
-// Last Update: 20210417
+// Last Update: 20210430
 // Description: Header file containing pin mappings for the Visualizer2 PCB
 
 #define L_CH_0 5
@@ -23,6 +23,17 @@
 #define MSG_STR 3
 
 #define COLOR_FSM 4
+#define NUM_COLOR_MODES 5
+#define FADE_DELAY 15
 
 #define L_CH_IN A6
 #define R_CH_IN A7
+
+#define DEBOUNCE_DELAY 50
+
+// Color schemes
+CRGB colors_1[7] = {CRGB::Red, CRGB(244, 140, 50), CRGB::Yellow, CRGB::Green, CRGB::Blue, CRGB::Indigo, CRGB::Red}; // Rainbow color scheme
+CRGB colors_2[7] = {CRGB::Blue, CRGB(0, 51, 204), CRGB(0, 102, 204), CRGB(0, 153, 204), CRGB(0, 204, 255), CRGB(102, 255, 255), CRGB::Blue}; // Ice color scheme
+CRGB colors_3[7] = {CRGB(128, 0, 0), CRGB(255, 0, 0), CRGB(255, 102, 0), CRGB(255, 204, 153), CRGB(255, 204, 0), CRGB(255, 255, 255), CRGB(128, 0, 0)}; // Heat color scheme
+CRGB colors_4[7] = {CRGB(15, 8, 75), CRGB(255, 229, 212), CRGB(191, 211, 193), CRGB(104, 166, 145), CRGB(105, 79, 93), CRGB(13, 2, 33), CRGB(15, 8, 75)}; // Midnight color scheme
+CRGB colors_5[7] = {CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0), CRGB(0, 0, 0)}; // Fade start
--- Visualizer2_SingleOutputCode/Visualizer2_SingleOutputCode.ino
+++ Visualizer2_SingleOutputCode/Visualizer2_SingleOutputCode.ino
@@ -4,11 +4,25 @@
 #define NUM_LEDS 210
 #define LED_SEG 30
 
+// LED strip
 CRGB all_leds[NUM_LEDS];
+CRGB channel_colors[7];
 
+// MSGEQ7 spectrum and adjusted spectrum
 int spectrum[7];
 int adj_spectrum[7];
 
+// For button FSM
+int colorBtnState;
+int lastColorBtnState = 0;
+unsigned long lastColorDebounceTime = 0;
+int colorMode = 0;
+
+// For fading colors
+int tLastRefresh = 0;
+int rFade = 255;
+int gFade = 0;
+int bFade = 0;
 
 void setup() {
 
@@ -34,35 +48,43 @@ void setup() {
   digitalWrite(MSG_STR, HIGH);
   delay(1);
 
+  // Set up color FSM 
+  pinMode(COLOR_FSM, INPUT);
+  assignArray(colors_1);
+
   Serial.begin(9600);
 
 }
 
 void loop() {
   FastLED.clear();
-  readAnalyzer();
-  adjustSpectrum();
+  readAnalyzer(); // Get MSGEQ7 Values
+  adjustSpectrum(); // Map MSGEQ7 values to number of LEDs
+  updateColorBtn(); // Sense if color button has been pressed
+  updateColors(); // Update colors to account for changing color schemes
+  updateFade(); // Update fade color scheme values
+
 
   for (int i = 0 + (0 * LED_SEG); i < adj_spectrum[0] ; i++) {
-    all_leds[i] = CRGB::Red;
+    all_leds[i] = channel_colors[0];
   }
   for (int i = (2 * LED_SEG); i > (2 * LED_SEG) - adj_spectrum[1]; i--) {
-    all_leds[i] = CRGB(244, 140, 50);
+    all_leds[i] = channel_colors[1];
   }
   for (int i = 0 + (2 * LED_SEG); i < adj_spectrum[2] + (2 * LED_SEG); i++) {
-    all_leds[i] = CRGB::Yellow;
+    all_leds[i] = channel_colors[2];
   }
   for (int i = (4 * LED_SEG); i > (4 * LED_SEG) - adj_spectrum[3]; i--) {
-    all_leds[i] = CRGB::Green;
+    all_leds[i] = channel_colors[3];
   }
   for (int i = 0 + (4 * LED_SEG); i < adj_spectrum[4] + (4 * LED_SEG); i++) {
-    all_leds[i] = CRGB::Blue;
+    all_leds[i] = channel_colors[4];
   }
   for (int i = (6 * LED_SEG); i > (6 * LED_SEG) - adj_spectrum[5]; i--) {
-    all_leds[i] = CRGB::Indigo;
+    all_leds[i] = channel_colors[5];
   }
   for (int i = 0 + (6 * LED_SEG); i < adj_spectrum[0] + (6 * LED_SEG); i++) {
-    all_leds[i] = CRGB::Red;
+    all_leds[i] = channel_colors[6];
   }
   FastLED.show();
   delay(25);
@@ -88,9 +110,93 @@ void readAnalyzer() {
 void adjustSpectrum() {
   for (int i = 0; i < 7; i++) {
     adj_spectrum[i] = map(spectrum[i], 50, 1023, 1, LED_SEG);
-    Serial.print(spectrum[i]);
-    Serial.print("  ");
+//    Serial.print(spectrum[i]);
+//    Serial.print("  ");
   }
-  Serial.println();
+//  Serial.println();
 
+}
+
+// Updates the "button pressed" variable (debounce FSM)
+void updateColorBtn() {
+  int reading = digitalRead(COLOR_FSM);
+  Serial.println(reading);
+  if (reading != lastColorBtnState) {
+    lastColorDebounceTime = millis();
+  }
+
+  Serial.println(millis() - lastColorDebounceTime);
+  if ((millis() - lastColorDebounceTime) > DEBOUNCE_DELAY) {
+    Serial.println("TRUE");
+    if (reading != colorBtnState) {
+      colorBtnState = reading;
+
+      if (colorBtnState == 1) {
+        circIncColorMode();
+        updateColors();
+        Serial.println("BTN PRESSED");
+      }
+    }
+  }
+  lastColorBtnState = reading;
+
+}
+
+// Updates the color mode state
+void updateColors() {
+  switch(colorMode) {
+    case 0: // RAINBOW
+      assignArray(colors_1);
+      break;
+    case 1: // ICE
+      assignArray(colors_2);
+      break;
+    break;
+    case 2: // HEAT
+      assignArray(colors_3);
+      break;
+    break;
+    case 3: // MIDNIGHT
+      assignArray(colors_4);
+      break;
+    break;
+    case 4: // FADE
+      assignArray(colors_5);
+    break;
+
+  }
+}
+
+// Copies color array
+void assignArray(CRGB arr[7]) {
+  for (int i = 0; i < 7; i++) {
+    channel_colors[i] = arr[i];
+  }
+}
+
+// Circular increment the color state
+void circIncColorMode() {
+  colorMode = (colorMode + 1) % NUM_COLOR_MODES;
+}
+
+// Circular increment for fading color schemes
+void updateFade() {
+  if (millis() - tLastRefresh > FADE_DELAY) {
+    if (rFade > 0 && bFade == 0) {
+      rFade--;
+      gFade++;
+    }
+    if (gFade > 0 && rFade == 0) {
+      gFade--;
+      bFade++;
+    }
+    if (bFade > 0 && gFade == 0) {
+      rFade++;
+      bFade--;
+    }
+    tLastRefresh = millis();
+  }
+  for (int i = 0; i < 7; i++) {
+    colors_5[i] = CRGB(rFade, gFade, bFade);
+  }
 }
